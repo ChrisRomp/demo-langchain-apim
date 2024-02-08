476a2db15227970cdd76f3bc5c709567fbe69eb9
Chris Romp <ChrisRomp@users.noreply.github.com>
2024-02-08 23:18:59 +0000

    Minor edits; implement py callback

--- apim.py
+++ apim.py
@@ -1,12 +1,11 @@
 from langchain.schema import HumanMessage
 from langchain_openai import AzureChatOpenAI
-from langchain.globals import set_verbose, set_debug
+from langchain_community.callbacks import get_openai_callback
+from langchain.globals import set_debug
 from dotenv import load_dotenv
 
 load_dotenv()
-
-set_verbose(False)
-set_debug(True)
+set_debug(False)
 
 model = AzureChatOpenAI(
     deployment_name="gpt-35-turbo-0613",
@@ -15,8 +14,16 @@ model = AzureChatOpenAI(
     streaming=False,
 )
 
-message = HumanMessage(
-    content="Translate this sentence from English to French: 'Hello, world!.'"
-)
+input_question = "What is the wing speed velocity of an unladen swallow?"
+print(f"\nInput: {input_question}")
 
-model.invoke(input=[message])
+message = HumanMessage(content=input_question)
+
+with get_openai_callback() as callback:
+    res = model.invoke(input=[message])
+    print(f"\nResponse: {res.content}\n")
+
+    print(res)
+
+    if model.streaming == False:
+        print(f"Callback:\n{callback}\n")
